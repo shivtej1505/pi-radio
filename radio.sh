5214766c78effacc936b72e984fca1903ce9f168
Shivang Nagaria <shivang.nagaria@gmail.com>
2023-10-02 15:14:32 +0530

    Added prev, next and reading stations from file

--- radio.sh
+++ radio.sh
@@ -3,7 +3,10 @@ source .env
 
 MPV_PID_FILE=/tmp/mpv.pid
 NC_INPUT_FILE=/tmp/nc_input
-
+IS_BLE_CONNECTED=0
+NUM_RADIO=`cat stations | wc -l | tr -d ' '`
+RADIO_IDX=1
+STREAM_URL=`cat stations | head -$RADIO_IDX | tail -1`
 
 kill_process() {
 	PID_FILE=$1
@@ -20,6 +23,34 @@ kill_process() {
 	fi
 }
 
+connect_ble() {
+	bluetoothctl connect "$SPEAKER_MAC_ADDR";
+	if [ $? -ne 0 ]; then
+		echo "Bluetooth connection failed"
+		exit 1
+	fi
+	IS_BLE_CONNECTED=1
+}
+
+next_radio_station() {
+	STREAM_URL=`cat stations | head -$RADIO_IDX | tail -1`;
+	RADIO_IDX=$((RADIO_IDX+1))
+	RADIO_IDX=$(($RADIO_IDX%$NUM_RADIO))
+
+	if [ $RADIO_IDX -eq 0 ]; then
+		RADIO_IDX=1
+	fi
+}
+
+previous_radio_station() {
+	STREAM_URL=`cat stations | head -$RADIO_IDX | tail -1`;
+	RADIO_IDX=$((RADIO_IDX-1))
+	RADIO_IDX=$(($RADIO_IDX%$NUM_RADIO))
+
+	if [ $RADIO_IDX -eq 0 ]; then
+		RADIO_IDX=1
+	fi
+}
 
 start() {
 	set -x;
@@ -29,20 +60,44 @@ start() {
 	do
 		echo "waiting for start....";
 		echo "HTTP/1.1 200 OK\n\n" | nc -l 0.0.0.0 $PORT -N > $NC_INPUT_FILE;
+
 		grep "start" $NC_INPUT_FILE;
-
-
 		if [ $? -eq 0 ];then
-			bluetoothctl connect "$SPEAKER_MAC_ADDR";
-			if [ $? -ne 0 ]; then
-				echo "Bluetooth connection failed"
-				exit 1
+			if [ $IS_BLE_CONNECTED -eq 0 ]; then
+				connect_ble;
 			fi
 
 			kill_process "$MPV_PID_FILE";
 			mpv -ao=pulse "$STREAM_URL" --volume=70 &
 			echo -n "$!" > $MPV_PID_FILE
 		fi
+
+
+		grep "next" $NC_INPUT_FILE;
+		if [ $? -eq 0 ];then
+			if [ $IS_BLE_CONNECTED -eq 0 ]; then
+				connect_ble;
+			fi
+
+			kill_process "$MPV_PID_FILE";
+			next_radio_station;
+			mpv -ao=pulse "$STREAM_URL" --volume=70 &
+			echo -n "$!" > $MPV_PID_FILE
+		fi
+
+
+		grep "prev" $NC_INPUT_FILE;
+		if [ $? -eq 0 ];then
+			if [ $IS_BLE_CONNECTED -eq 0 ]; then
+				connect_ble;
+			fi
+
+			kill_process "$MPV_PID_FILE";
+			previous_radio_station;
+			mpv -ao=pulse "$STREAM_URL" --volume=70 &
+			echo -n "$!" > $MPV_PID_FILE
+		fi
+
 
 		grep "stop" $NC_INPUT_FILE;
 		if [ $? -eq 0 ];then
